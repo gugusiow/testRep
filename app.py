185d36b45fdbebf0f9cd4a44e820753df14ce10d
Pearlynn Khoo <pearlynn@Pearlynns-MacBook-Air.local>
2025-09-06 11:23:43 +0800

    commit

--- app.py
+++ app.py
@@ -2,14 +2,20 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def home():
-    return "Welcome to the Flask app!"
+@app.route('/trivia', methods=['GET'])
+# def home():
+#     return "Welcome to the Flask app!"
 
-@app.route('/data', methods=['POST'])
-def receive_data():
-    data = request.get_json()
-    return jsonify({"received": data}), 201
+# @app.route('/data', methods=['POST'])
+# def receive_data():
+#     data = request.get_json()
+#     return jsonify({"received": data}), 201
+
+def get_trivia():
+    # Answers to the trivia questions
+    answers = [2, 1, 2, 2, 3, 4, 3, 5, 4]
+    
+    return jsonify({"answers": answers})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
